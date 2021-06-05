af8d1434c8f04f4617cdb42cb4dc54ba9ab2f0e3
prince-deb <78799922+prince-deb@users.noreply.github.com>
2021-06-05 22:02:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,22 @@ var Content=event.results[0][0].transcript;
 console.log(Content);
 
 document.getElementById("textbox").innerHTML=Content;
+if(Content=="take my selfie"){
+    console.log("taking selfie");
+    speak();
 }
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textbox").value;
+    var utterThis =new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    webcam.attach(camera);
+}
+camera=document.getElementById("camera");
+webcam.set({
+    width:400,
+    height:300,
+    image_format:'jpg',
+    jpg_quality:90
+});
