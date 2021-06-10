b75d0307703faa7b6167cdb028ca6f67e64fd653
prince-deb <78799922+prince-deb@users.noreply.github.com>
2021-06-10 22:05:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-var SpeechRecognition=window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
-function start()
-{
-    document.getElementById("textbox").innerHTML="";
-    recognition.start();
-}
+var SpeechRecognition = window.webkitSpeechRecognition;
+ var recognition = new SpeechRecognition(); 
+ function start() { document.getElementById("textbox").innerHTML = ""; 
+ recognition.start(); } recognition.onresult = function(event) { console.log(event);
+     var Content = event.results[0][0].transcript;
+      document.getElementById("textbox").innerHTML = Content; console.log(Content); if(Content =="selfie") 
+    { console.log("taking selfie --- "); speak(); } } 
+    function speak(){ var synth = window.speechSynthesis; speak_data = "Taking you Selfie in 5 seconds";
+     var utterThis = new SpeechSynthesisUtterance(speak_data); synth.speak(utterThis); Webcam.attach(camera);
+      setTimeout(function() { take_snapshot(); save(); }, 5000); } camera = document.getElementById("camera");
+       Webcam.set({ width:360, height:250, image_format : 'jpeg', jpeg_quality:90 });
 
-recognition.onresult=function(event){
-    console.log(event);
-var Content=event.results[0][0].transcript;
-console.log(Content);
-
-document.getElementById("textbox").innerHTML=Content;
-if(Content=="take my selfie"){
-    console.log("taking selfie");
-    speak();
+       function take_snapshot()
+        { Webcam.snap(function(data_uri) 
+            { document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>'; }); }
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
 }
-}
-function speak(){
-    var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
-    var utterThis =new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-    webcam.attach(camera);
-}
-camera=document.getElementById("camera");
-webcam.set({
-    width:400,
-    height:300,
-    image_format:'jpg',
-    jpg_quality:90
-});
