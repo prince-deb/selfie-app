84176b1c963bcb8e17f9e03273f600197d15f47f
prince-deb <78799922+prince-deb@users.noreply.github.com>
2021-06-12 12:52:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var SpeechRecognition = window.webkitSpeechRecognition;
     function speak(){ var synth = window.speechSynthesis; speak_data = "Taking you Selfie in 5 seconds";
      var utterThis = new SpeechSynthesisUtterance(speak_data); synth.speak(utterThis); Webcam.attach(camera);
       setTimeout(function() { take_snapshot(); save(); }, 5000); } camera = document.getElementById("camera");
-       Webcam.set({ width:360, height:250, image_format : 'jpeg', jpeg_quality:90 });
+       Webcam.set({ width:100, height:200, image_format : 'jpeg', jpeg_quality:90 });
 
        function take_snapshot()
         { Webcam.snap(function(data_uri) 
